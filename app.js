63f2a3590b5b400f9cde8f8dade8356037957f3d
Gilbert Vincenta <gilbert11323@gmail.com>
2019-04-04 19:29:49 +1100

    added package-lock.json

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-{
-  "name": "ez-life",
-  "version": "1.0.0",
-  "description": "INFO30005 2019 Semester 1 Project.",
-  "main": "app.js",
-  "scripts": {
-    "test": "echo \"Error: no test specified\" && exit 1"
-  },
-  "author": "Gilbert Vincenta, Firman Otto, Erya Wen, Garvin Prathama Bulkin",
-  "license": "ISC"
-}
+// Set up express
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+// Database setup
+require('./models/db.js');
+
+
+// Routes setup
+var routes = require('./routes/routes.js');
+app.use('/',routes);
+
+
+
+// Start the server
+app.listen(4000,function(req,res){
+	console.log('Express listening on port 4000');
+});
