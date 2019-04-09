b26953cbc8b9d63d534b79ec226a5debf0a1e8e1
Gilbert Vincenta <gilbert11323@gmail.com>
2019-04-09 15:29:06 +1000

    updated app.ks

--- app.js
+++ app.js
@@ -10,10 +10,10 @@ const PORT = process.env.PORT || 3000;
 // Routes setup
 var routes = require('./routes/routes.js');
 app.use('/',routes);
-
-
-
+// users setup 
+var users = require("./routes/users.js");
+app.use("/users",users);
 // Start the server
 app.listen(PORT,function(req,res){
-	console.log('Express listening on port ${PORT}');
+	console.log(`Express listening on port ${PORT}`);
 });
