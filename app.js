57d04b25753f7d8b0ebfcd3121aebfb4a4d695e9
Gilbert Vincenta <gilbert11323@gmail.com>
2019-04-27 15:39:34 +1000

    changed start directory

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ app.use("/api/report", report);
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
-
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
