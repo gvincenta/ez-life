9cf08e3ecd69a1d4d7c3037e6e31c14f7ec76630
garviin <garvibulkin@gmail.com>
2019-05-22 00:13:09 +1000

    new sign-in page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
 import { Switch, Route } from "react-router-dom";
+import { Button, FormGroup, FormControl, FormLabel } from "react-bootstrap";
 import axios from "axios";
 import "./App.css";
 import NavBar from "./Header";
+import SignIn from "./SignIn";
 import Report from "./Report";
 import SideBar from "./sidebar";
 import TransList from "./transaction/transList";
@@ -51,38 +53,7 @@ class App extends Component {
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value });
   };
-  /** login a user */
-  handleLogIn = event => {
-    this.setState({ error: {} });
-    var self = this;
-    var path;
-    //for existing user, signin:
-    if (self.state.existingUser === true) {
-      path = "/users/signin";
-    }
-    //for new user, signup:
-    else {
-      path = "/users/signup";
-    }
-    //ask from backend:
-
-    axios
-      .post(path, { email: self.state.username, password: self.state.password })
-      .then(response => {
-        //set token:
-        self.setState({ token: response.data.token });
-        //setup axios: 
-        UserProfile.setName(response.data.token);
-        axios.defaults.headers.common["Authorization"] = response.data.token; 
-
-        //reload page after logging in:
-        window.location.reload();
-        //catch any errors:
-      })
-      .catch(err => {
-        alert("password/username incorrect OR may already have signed up.");
-      });
-  };
+  
   //user log out: 
   handleLogOut = event =>{
     //remove persisting token: 
@@ -104,53 +75,7 @@ class App extends Component {
     if (this.state.token.length === 0) {
       var err = this.state.error;
       res = (
-        <div >
-          <NavBar signedIn = {false}/>
-          <form>
-            <input
-              type="checkbox"
-              value="I am an existing user"
-              onClick={this.isExistingUser}
-            />< label> I am an existing user </label> <br /><br />
-            <h2>Sign Up / Log In </h2>
-            
-            <div class="input-container">
-                <i class="fa fa-envelope icon"></i>
-
-    <label> email/username: </label>
-          <input
-            type="text"
-            name="username"
-            value={this.state.username}
-            onChange={this.handleChange}
-            placeholder="not case sensitive"
-          />
-          </div>
-
-  <div class="input-container">
-    <i class="fa fa-key icon"></i>
-          <label> password: </label>
-
-          <input
-            type="password"
-            name="password"
-            value={this.state.password}
-            onChange={this.handleChange}
-            placeholder="case sensitive"
-          />
-
-  </div>
-
-  <button name = "login" type="button" class="btn btn-secondary" onClick = {this.handleLogIn}> Log In </button>
-  
-          </form></div>
-
-          
-
-          
-
-          
-
+          <SignIn/>
       );
     } else {
       res = (
